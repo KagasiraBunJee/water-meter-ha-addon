89d2637aabfdc13d0bb166ebdfcdc98263a1a749
KagasiraBunJee <kagasirabunjee@yahoo.com>
2024-01-07 02:36:34 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -16,41 +16,41 @@ DBNAME=$(bashio::config 'mongo_db_name')
 
 bashio::log.info "Start mongod service"
 # apply mongo script in free access mode
-MONGO_RESULT=$(mongod --config mongo.conf --dbpath /data/mongodb --port $DBPORT --fork)
-bashio::log.info "$MONGO_RESULT"
-MONGO_PID=$(echo "$MONGO_RESULT" | grep -o 'forked process: [0-9]*' | awk '{print $NF}')
-mongosh --port 8084 --eval "let adminDB = db.getSiblingDB('admin');
-                let usersList = adminDB.getUsers();
-                if (usersList.users.length === 0) {
-                  print(adminDB.createUser({
-                      user: '$DBLOGIN',
-                      pwd: '$DBPASS',
-                      roles: [
-                          {
-                              role: 'readWrite',
-                              db: '$DBNAME',
-                          },
-                      ],
-                  }));
-                }"
-sleep 2
-kill -15 "$MONGO_PID" # stop mongo
-sleep 2
-
-bashio::log.info "Start secured mongod service"
-# run mongo in auth mode
-mongod --config mongo.conf --dbpath /data/mongodb --port 8084 --bind_ip_all --auth --fork
-
-cp -a server/. /data/server/
-
-cat <<EOF >/data/server/.env
-API_TOKEN=${SERVER_API_TOKEN}
-PORT=${SERVER_PORT}
-DBLOGIN=${DBLOGIN}
-DBPASS=${DBPASS}
-DBNAME=${DBNAME}
-DBDOMAIN=${DBDOMAIN}
-DBPORT=${DBPORT}
-EOF
-
-sh /prepare-server.sh
+mongod --config mongo.conf --dbpath /data/mongodb --port $DBPORT --fork
+#MONGO_RESULT=$(mongod --config mongo.conf --dbpath /data/mongodb --port $DBPORT --fork)
+#echo "$MONGO_RESULT"
+#MONGO_PID=$(echo "$MONGO_RESULT" | grep -o 'forked process: [0-9]*' | awk '{print $NF}')
+#mongosh --port 8084 --eval "let adminDB = db.getSiblingDB('admin');
+#                let usersList = adminDB.getUsers();
+#                if (usersList.users.length === 0) {
+#                  print(adminDB.createUser({
+#                      user: '$DBLOGIN',
+#                      pwd: '$DBPASS',
+#                      roles: [
+#                          {
+#                              role: 'readWrite',
+#                              db: '$DBNAME',
+#                          },
+#                      ],
+#                  }));
+#                }"
+#sleep 2
+#kill -15 "$MONGO_PID" # stop mongo
+#sleep 2
+#
+## run mongo in auth mode
+#mongod --config mongo.conf --dbpath /data/mongodb --port 8084 --bind_ip_all --auth --fork
+#
+#cp -a server/. /data/server/
+#
+#cat <<EOF >/data/server/.env
+#API_TOKEN=${SERVER_API_TOKEN}
+#PORT=${SERVER_PORT}
+#DBLOGIN=${DBLOGIN}
+#DBPASS=${DBPASS}
+#DBNAME=${DBNAME}
+#DBDOMAIN=${DBDOMAIN}
+#DBPORT=${DBPORT}
+#EOF
+#
+#sh /prepare-server.sh
